051b48cd04f7f4f7bc0f86f766ef9045e2dacb93
Vaishnavi Devi V <vaisuvenkat2000@gmail.com>
2022-06-16 09:55:59 +0530

    Updated Weatherforecast

--- Azure_CICD/Controllers/WeatherForecastController.cs
+++ Azure_CICD/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Azure_CICD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing",  "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
